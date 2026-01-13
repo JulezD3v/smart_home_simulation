dfae15d4dc44057b13c1ad2de2b20261f92df724
JulezD3v <jul3zdev@gmail.com>
2026-01-14 00:38:53 +0300

    runs on main only

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include <Arduino.h>
+// int red = 7;
+// void setup() {
+//   pinMode(red, OUTPUT);   // Built-in LED
+// }
 
-void setup() {
-  pinMode(13, OUTPUT);   // Built-in LED
+// void loop() {
+//   digitalWrite(red, HIGH);
+//   digitalWrite(8,LOW);
+//   delay(1000);
+//   digitalWrite(red, LOW);
+//   digitalWrite(8, HIGH);
+//   delay(1000);
+// }
+
+// motion sensored fun
+
+#include <Arduino.h>
+//#include "fun.h"
+
+int motorA = 10;
+int motorB = 11;
+
+void setup(){
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+void loop(){
+    digitalWrite(10, HIGH);
+    digitalWrite(11, LOW);
+
+    delay(3000);
+
+    digitalWrite(10, LOW);
+    digitalWrite(11, HIGH);
+    delay(3000);
+
+    digitalWrite(10, LOW);
+    digitalWrite(11, LOW);
+    delay(4000);
+
 }
+
+//void loop(){} Fun still running
